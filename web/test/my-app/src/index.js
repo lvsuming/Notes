574b67bf70ca58c7760b9b75344fcef71e4ced4d
v-lvsm01 <v-lvsm01@vanke.com>
2017-06-19 19:30:05 +0800

    ..

--- web/test/my-app/src/index.js
+++ web/test/my-app/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ProvinceCityDistrict from './demo-provinceCityDistrict';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+ReactDOM.render(<ProvinceCityDistrict />, document.getElementById('provinceCityDistrict'));
 ReactDOM.render(<App />, document.getElementById('root'));
+
 registerServiceWorker();
