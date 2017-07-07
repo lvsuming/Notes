0ea68ee69f61ee91ff7cab3e22fff1e016305daa
v-lvsm01 <v-lvsm01@vanke.com>
2017-07-07 18:25:41 +0800

    add fetch Function

--- web/test/my-app/src/components/homeBoard/homeBoard.jsx
+++ web/test/my-app/src/components/homeBoard/homeBoard.jsx
@@ -1,17 +1,66 @@
 import React from 'react';
 import {Link} from 'react-router';
 import  './homeBoard.css';
+import base from '../base';
 class HomeBoard extends React.Component{
+    getMytodoNum(){
+        var _this = this;
+        var body = 'type=all&pagenum';
+        fetch( base.getUrl.getMytodoNum+'&'+body , {
+            method : "GET",
+            credentials: 'include'
+        }).then(function (res) {
+            if (res.ok) {
+                res.json().then(function (resultData) {
+                    if(resultData.data && resultData.data.length>0){
+                        _this.setState({
+                            myTodoNum : JSON.parse(resultData.data).num
+                        });
+                    }
+                });
+            } else {
+                alert("Oops! You are not authorized.");
+            }
+        }, function (e) {
+            alert("Error submitting form!");
+        });
+    }
+    getMyprocessNum(){
+        var _this = this;
+        var body = 'type=all&pagenum';
+        fetch( base.getUrl.getMyprocessNum+'&'+body , {
+            method : "GET",
+            credentials: 'include'
+        }).then(function (res) {
+            if (res.ok) {
+                res.json().then(function (resultData) {
+                    if(resultData.data && resultData.data.length>0){
+                        _this.setState({
+                            myProcessNum : JSON.parse(resultData.data).num
+                        })
+                    }
+                });
+            } else {
+                alert("Oops! You are not authorized.");
+            }
+        }, function (e) {
+            alert("Error submitting form!");
+        });
+    }
+    componentDidMount(){
+        this.getMytodoNum();
+        this.getMyprocessNum();
+    };
     render(){
         return <ul className="home-board bg-white align-center">
             <a href='/sys/notify/mobile/index.jsp'>
                 <li className="grid-2 fl">
-                    <div className="todolist"><div className="num hide"></div>待办事宜</div>
+                    <div className="todolist"><div className="num hide">{this.props.myTodoNum || 0}</div>待办事宜</div>
                 </li>
             </a>
             <Link to='/myFlow' >
                 <li className="grid-2 fl">
-                    <div className="myprocess"><div className="num hide">2</div>我的流程</div>
+                    <div className="myprocess"><div className="num hide">{this.props.myProcessNum || 0}</div>我的流程</div>
                 </li>
             </Link>
         </ul>
--- web/test/my-app/src/components/homeFlow/homeFlow.jsx
+++ web/test/my-app/src/components/homeFlow/homeFlow.jsx
@@ -21,9 +21,7 @@ class Module extends React.Component{
         }
     }
     getList(){
-        this.setState(
-            {'dataList':[{"fdId":"15ceda95d29687035871b014725aa09a","docSubject":"现金贷合同撤消-12873723001","docCreateTime":"2017-06-28","docLink":"/km/review/km_review_main/kmReviewMain.do?method=view&fdId=15ceda95d29687035871b014725aa09a","fdCategoryName":"现金货合同撤销申请表","docStatus":"结束","docReadCount":0,"docCreatorName":"李烁"},{"fdId":"15ced9fc885fed637848659462696e97","docSubject":"合同撤销","docCreateTime":"2017-06-28","docLink":"/km/review/km_review_main/kmReviewMain.do?method=view&fdId=15ced9fc885fed637848659462696e97","fdCategoryName":"合同状态修改申请","docStatus":"结束","docReadCount":2,"docCreatorName":"欧阳思洁"},{"fdId":"15ced9b9c16fe183b3af4bd4e2a86f3a","docSubject":"出差申请","docCreateTime":"2017-06-28","docLink":"/km/review/km_review_main/kmReviewMain.do?method=view&fdId=15ced9b9c16fe183b3af4bd4e2a86f3a","fdCategoryName":"出差申请","docStatus":"待审","docReadCount":0,"docCreatorName":"夏兴平"},{"fdId":"15ced9aa089d1bddfea6b9740ffb37a4","docSubject":"现金贷合同撤消-12874055001","docCreateTime":"2017-06-28","docLink":"/km/review/km_review_main/kmReviewMain.do?method=view&fdId=15ced9aa089d1bddfea6b9740ffb37a4","fdCategoryName":"现金货合同撤销申请表","docStatus":"结束","docReadCount":1,"docCreatorName":"李烁"},{"fdId":"15ced917811c11be6d70fcf4b278ac1c","docSubject":"出差申请","docCreateTime":"2017-06-28","docLink":"/km/review/km_review_main/kmReviewMain.do?method=view&fdId=15ced917811c11be6d70fcf4b278ac1c","fdCategoryName":"出差申请","docStatus":"待审","docReadCount":0,"docCreatorName":"夏兴平"}]
-        });
+        var _this = this;
         var body = 'type=all&pagenum='+this.state.pagenum+'&rowsize='+this.state.rowsize;
         /*var para = {
             'type':'all',
@@ -41,22 +39,31 @@ class Module extends React.Component{
         }).then(function(json) {
             console.log(json);
         });*/
+        /*let formData = new FormData();
+        formData.append('type', 'all');
+        formData.append('pagenum', this.state.pagenum);
+        formData.append('rowsize', this.state.rowsize);*/
         fetch( base.getUrl.getMyFlowList+'&'+body , {
-            method : "GET"
+            method : "GET",
+            credentials: 'include'
         }).then(function (res) {
             if (res.ok) {
-                console.log(JSON.stringify(res));
-            } else if (res.status === 401) {
-                alert("Oops! You are not authorized.");
+                res.json().then(function (resultData) {
+                    if(resultData.data && resultData.data.length>0){
+                        _this.setState(
+                            {'dataList': resultData.data}
+                        );
+                    }
+                });
+            } else {
+                console.error("Oops! You are not authorized.");
             }
         }, function (e) {
-            alert("Error submitting form!");
+            console.error("Error submitting form!");
         });
-
-
     }
     componentDidMount(){
-        this.getList()
+        this.getList();
     };
     render(){
         return (<ul className="home-news bg-white clr">
